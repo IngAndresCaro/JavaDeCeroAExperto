94e6020d069b6a49c57909d149d7fbc54c9b840c
Andres Caro <andrcagl@suramericana.com.co>
2025-05-23 14:12:10 -0500

    se termina la parte basica

--- src/Argumentos/ArgumentoLineaComandoCalculadora.java
+++ src/Argumentos/ArgumentoLineaComandoCalculadora.java
@@ -17,34 +17,49 @@ public class ArgumentoLineaComandoCalculadora {
             System.exit(-1);
         }
         String operacion = args[0];
-        int a = Integer.parseInt(args[1]);
-        int b = Integer.parseInt(args[2]);
-        double resultado = 0;
 
-        switch (operacion){
-            case "suma":
-                resultado = a + b;
-                System.out.println("La suma es: " + resultado);
-                break;
-            case "resta":
-                resultado = a - b;
-                System.out.println("La resta es: " + resultado);
-                break;
-            case "multi":
-                resultado = a * b;
-                System.out.println("La multiplicación es: " + resultado);
-                break;
-            case "div":
-                if (b != 0) {
-                    resultado = (double) a / b;
-                    System.out.println("La división es: " + resultado);
-                } else {
-                    System.out.println("Error: División por cero.");
-                    System.exit(-1);
-                }
-                break;
-            default:
-                System.out.println("Operación no válida.");
+        try {
+            int a = Integer.parseInt(args[1]);
+            int b = Integer.parseInt(args[2]);
+            double resultado = 0;
+
+            switch (operacion){
+                case "suma":
+                    resultado = a + b;
+                    System.out.println("La suma es: " + resultado);
+                    break;
+                case "resta":
+                    resultado = a - b;
+                    System.out.println("La resta es: " + resultado);
+                    break;
+                case "multi":
+                    resultado = a * b;
+                    System.out.println("La multiplicación es: " + resultado);
+                    break;
+                case "div":
+                    if (b != 0) {
+                        resultado = (double) a / b;
+                        System.out.println("La división es: " + resultado);
+                    } else {
+                        System.out.println("Error: División por cero.");
+                        System.exit(-1);
+                    }
+                    break;
+                default:
+                    System.out.println("Operación no válida.");
+            }
+        } catch (NumberFormatException e) {
+            System.err.println("Error: Debe ingresar números enteros.");
+            System.exit(-1);
+        } catch (ArithmeticException e) {
+            System.err.println("Error: División por cero.");
+            System.exit(-1);
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(-1);
+
+
+
         }
 
     }
